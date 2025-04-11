db8e2286682c739af01e9855cb5d9887fa0a5de7
davi <davifernadesr@gmail.com>
2025-04-10 21:52:59 -0300

    criação da função para criar  a tabela de usuarios

--- repository/querysRepository.go
+++ repository/querysRepository.go
@@ -21,17 +21,18 @@ func CriaTabelaUser(db *sql.DB) error{
 				email text not null,
 				password text not null,
 				balance integer not null,
-				userType text not null
+				userType text not null,
+				unique(document, email)
 			);
 			`
 
 			_, err:= db.Exec(query)
 			if err != nil {
 
-				return fmt.Errorf("erro ao criar a tabela: %v", err.Error())
+				return fmt.Errorf("erro ao criar a tabela users: %v", err.Error())
 			}
 
-			fmt.Println("tabela criada com sucesso")
+			fmt.Println("tabela criada users com sucesso")
 			return nil
 
 }
@@ -40,6 +41,28 @@ func CriaTabelaUser(db *sql.DB) error{
 //amanha faço
 func CriaTabelaTransaction(db *sql.DB) error{
 
+	query:= `
+	
+			create table if not exists transfer (
+			
+				id integer primary key autoincrement,
+				amount integer not null,
+				senderID integer not null,
+				receiverID integer not null,
+				localDaetTime text not null,
+				foreign key (senderID) references users(id),
+				foreign key (receiverID) references users(id)
+
+			);
+
+	`
+
+	_,err:= db.Query(query)
+	if err != nil{
+		return fmt.Errorf("erro ao criar a tabela transfer: %v", err.Error())
+	}
+
+	fmt.Println("tabela transfer criada com sucesso")
 
 	return nil
 }
