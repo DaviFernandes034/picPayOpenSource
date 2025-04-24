c7e1e82f8965b83697912063d111e50ba89e7ce4
davi <davifernadesr@gmail.com>
2025-04-24 12:59:41 -0300

    funcionando ok

--- repository/init.go
+++ repository/init.go
@@ -11,10 +11,13 @@ import (
 func Init()(*sql.DB, error){
 
 
-	db, err:= sql.Open("sqlite3", ":memory:")
+	db, err:= sql.Open("sqlite3", "meuBanco.db")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Ativa foreign keys 
+	db.Exec("PRAGMA foreign_keys = ON")
 
 	return db, nil
 }
