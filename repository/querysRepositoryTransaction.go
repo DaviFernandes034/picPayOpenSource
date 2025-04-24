21b56ca2727dfa2d137aa06f85de79310ca5cf60
davi <davifernadesr@gmail.com>
2025-04-24 13:00:46 -0300

    modificações feitas para trazer os dados certinhos

--- repository/querysRepositoryTransaction.go
+++ repository/querysRepositoryTransaction.go
@@ -7,9 +7,11 @@ import (
 )
 
 type TransactionRepository interface {
-	CriaTabelaTransaction(db *sql.DB) error
+	CriaTabelaTransaction() error
+	Create(model model.Transaction) error
+	findTransferById(id int) (model.Transaction, error)
 	Save(model model.Transaction) error
-	findTrasnferById(id int) (model.Transaction, error)
+	AllTranfer() ([]model.Transaction, error)
 }
 
 type transactionRepository struct {
@@ -17,24 +19,88 @@ type transactionRepository struct {
 }
 
 
+
+// Save implements TransactionRepository.
+func (t *transactionRepository) Save(model model.Transaction) error {
+
+	panic("yuyfd")
+}
+
 func NewtypeRepositoryTransfer(db *sql.DB) TransactionRepository {
 
 	return &transactionRepository{db: db}
 }
 
+// AllTranfer implements TransactionRepository.
+func (t *transactionRepository) AllTranfer() ([]model.Transaction, error) {
+	
+	query:= `select id, amount, senderID, receiverID, localDateTime from transfer `
+	
+	rows, err:= t.db.Query(query)
+	if err != nil {
+
+		return nil, fmt.Errorf("erro na query: %w", err)
+	}
+
+	defer rows.Close()
+
+	var tranfers []model.Transaction
+
+	for rows.Next() {
+
+		var tranfer model.Transaction
+
+		if err:= rows.Scan(
+
+			&tranfer.TransferID,
+			&tranfer.Amount,
+			&tranfer.SenderID,
+			&tranfer.ReceiverID,
+			&tranfer.LocaldateTime); err != nil {
+
+				return nil, fmt.Errorf("erro ao passar os dados para a struct transactio: %w", err)
+
+			}
+
+			tranfers = append(tranfers, tranfer)
+	}
+	
+	if err:= rows.Err(); err != nil {
+		return nil, fmt.Errorf("erro entre as iterações: %w", err)
+	}
+
+	return tranfers , nil
+
+}
+
 
 // Save implements TransactionRepository.
-func (t *transactionRepository) Save(model model.Transaction) error {
-	panic("unimplemented")
+func (t *transactionRepository) Create(model model.Transaction) error {
+
+	query := `insert into transfer(amount, senderID, receiverID, localDateTime) values (? , ? , ? ,?)`
+
+	stmt, err := t.db.Prepare(query)
+	if err != nil {
+
+		return fmt.Errorf("erro ao preparar o stmt: %v", err)
+	}
+
+	_, err = stmt.Exec(model.Amount, model.SenderID, model.ReceiverID, model.LocaldateTime)
+	if err != nil {
+
+		return fmt.Errorf("erro ao executar a query: %v", err)
+	}
+
+	return nil
 }
 
 // findTrasnferById implements TransactionRepository.
-func (t *transactionRepository) findTrasnferById(id int) (model.Transaction, error) {
+func (t *transactionRepository) findTransferById(id int) (model.Transaction, error) {
 	panic("unimplemented")
 }
 
 // CriaTabelaTransaction implements TransactionRepository.
-func (t *transactionRepository) CriaTabelaTransaction(db *sql.DB) error {
+func (t *transactionRepository) CriaTabelaTransaction() error {
 
 	query := `
 		
@@ -44,7 +110,7 @@ func (t *transactionRepository) CriaTabelaTransaction(db *sql.DB) error {
 			amount integer not null,
 			senderID integer not null,
 			receiverID integer not null,
-			localDaetTime text not null,
+			localDateTime text not null,
 			foreign key (senderID) references users(id),
 			foreign key (receiverID) references users(id)
 
@@ -52,7 +118,7 @@ func (t *transactionRepository) CriaTabelaTransaction(db *sql.DB) error {
 
 	`
 
-	_, err := db.Query(query)
+	_, err := t.db.Exec(query)
 	if err != nil {
 		return fmt.Errorf("erro ao criar a tabela transfer: %v", err.Error())
 	}
--- repository/querysRepositoryUsers.go
+++ repository/querysRepositoryUsers.go
@@ -4,12 +4,15 @@ import (
 	"database/sql"
 	"desafio-pic-pay-open-source/model"
 	"fmt"
+
 )
 
 type UserRepository interface {
-	CriaTabelaUser(db *sql.DB) error
+	CriaTabelaUser() error
 	FindUserById(id int) (model.User, error)
+	Create(model model.UserDTO) error
 	Save(model model.User) error
+	UserAll() ([]model.User, error)
 }
 
 type userRepository struct {
@@ -17,24 +20,133 @@ type userRepository struct {
 }
 
 
-
 func NewtypeRepository(db *sql.DB) UserRepository {
 
 	return &userRepository{db: db}
 }
+
+// UserAll implements UserRepository.
+func (u *userRepository) UserAll() ([]model.User, error) {
+
+	query:= "select id, name, lastName,document, email, password, balance, userType from users"
+
+	rows, err:= u.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("erro na query: %w", err)
+	}
+
+	defer rows.Close()
+
+	var users []model.User
+
+	for rows.Next(){
+
+		var user model.User
+
+		if err:= rows.Scan(
+				
+			&user.UserId,
+			&user.Name,
+			&user.LastName,
+			&user.Document,
+			&user.Email,
+			&user.Password,
+			&user.Balance,
+			&user.UserType); err != nil {
+
+				return nil, fmt.Errorf("erro ao passar os dados para a struct user, %w", err)
+			}
+
+			users = append(users, user)
+	}
+
+	if err:= rows.Err(); err != nil {
+		return nil, fmt.Errorf("erro entre as iterações: %w", err)
+	}
+
+
+	return users, nil
+}
+
+
+
 // Save implements UserRepository.
 func (u *userRepository) Save(model model.User) error {
-	panic("unimplemented")
+
+	query := `update users
+			set balance = ?
+			where id = ?`
+
+	stmt, err := u.db.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("erro ao preparar a query; %v", err)
+	}
+
+	_, err = stmt.Exec(model.Balance, model.UserId)
+	if err != nil {
+		return fmt.Errorf("erro ao executar o stmt: %v", err)
+	}
+
+	return nil
+
+}
+
+// Create implements UserRepository.
+func (u *userRepository) Create(model model.UserDTO) error {
+
+	query := `insert into users(name, lastName,document, email, password, balance, userType)
+			  values (?,?,?,?,?,?,?)`
+
+	stmt, err := u.db.Prepare(query)
+	if err != nil {
+		return fmt.Errorf("erro ao preparar a query; %v", err)
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(model.Name, model.LastName, model.Document, model.Email, model.Password, model.Balance, model.UserType)
+	if err != nil {
+		return fmt.Errorf("erro ao executar o stmt: %v", err)
+	}
+
+	return nil
 }
 
 // FindUserById implements UserRepository.
 func (u *userRepository) FindUserById(id int) (model.User, error) {
-	panic("unimplemented")
+
+	query := `select id, name, lastName,document, email, password, balance, userType from users 
+				where id = ?`
+
+	var user model.User
+
+	err := u.db.QueryRow(query, id).Scan(
+
+		&user.UserId,
+		&user.Name,
+		&user.LastName,
+		&user.Document,
+		&user.Email,
+		&user.Password,
+		&user.Balance,
+		&user.UserType,
+	)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			return model.User{}, fmt.Errorf("usuario com id %d não encontrado", id)
+		}
+
+		return model.User{}, fmt.Errorf("erro ao buscar usuario: %v", err)
+	}
+
+	return user, nil
+
 }
 
 //aqui é aonde ficrá as querys do banco de dados em memoria
 
-func (u *userRepository) CriaTabelaUser(db *sql.DB) error {
+func (u *userRepository) CriaTabelaUser() error {
 
 	query := `
 			create table if not exists users (
@@ -51,7 +163,7 @@ func (u *userRepository) CriaTabelaUser(db *sql.DB) error {
 			);
 			`
 
-	_, err := db.Exec(query)
+	_, err := u.db.Exec(query)
 	if err != nil {
 
 		return fmt.Errorf("erro ao criar a tabela users: %v", err.Error())
@@ -61,5 +173,3 @@ func (u *userRepository) CriaTabelaUser(db *sql.DB) error {
 	return nil
 
 }
-
-
